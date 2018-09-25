ebacf98d1a0f7b08dad522714b506107edb83853
Peter Kreppein <pmkreppein@gmail.com>
2018-09-25 22:02:34 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,10 +5,24 @@ def reformat_languages(languages)
       :ruby << [:style] = [:oo]
    
     
+    
      when :javascript
       :javascript << [:style] = [:oo, :functional]
     
+    when :python
+      :python << [:style] = [:oo]
+      
+     when :java
+      :java << [:style] = [:oo]  
     
-    
-    
+     when :clojure  
+      :clojure << [:style] = [:functional]
+      
+     when :erlang
+      :erlang << [:style] = [:functional]
+      
+     when :scala
+      :scala << [:style] = [:functional]
+      
+    end
 end
