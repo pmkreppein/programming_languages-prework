d57249222d0aa8c1dab07969ca666c4248cf417e
Peter Kreppein <pmkreppein@gmail.com>
2018-09-25 22:02:45 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
 def reformat_languages(languages)
-  # your code here
+  languages.map |language|
+    case language 
+    when :ruby
+      :ruby << [:style] = [:oo]
+   
+    
+    
+     when :javascript
+      :javascript << [:style] = [:oo, :functional]
+    
+    when :python
+      :python << [:style] = [:oo]
+      
+     when :java
+      :java << [:style] = [:oo]  
+    
+     when :clojure  
+      :clojure << [:style] = [:functional]
+      
+     when :erlang
+      :erlang << [:style] = [:functional]
+      
+     when :scala
+      :scala << [:style] = [:functional]
+      
+    end
 end
